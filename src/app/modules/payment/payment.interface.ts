505411d3162e745e168274b9a1b2a190f4d78160
Sabithasan2098 <sabithasan2098@gmail.com>
2025-01-11 09:15:14 +0600

    add payment.section

---
+++ src/app/modules/payment/payment.interface.ts
@@ -0,0 +1,14 @@
+export type TPayment = {
+  January?: string;
+  February?: string;
+  March?: string;
+  April?: string;
+  May?: string;
+  June?: string;
+  July?: string;
+  August?: string;
+  September?: string;
+  October?: string;
+  November?: string;
+  December?: string;
+};
